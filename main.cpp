7c6059a3b48c05ae223add45ed93f00fe36e05e8
davidcspence3 <davidcspence3@gmail.com>
2020-08-01 23:47:25 +0000

    finished assignment

--- main.cpp
+++ main.cpp
@@ -18,12 +18,12 @@ video: Chapter 2 - Part 3
  
  
  
- 
- 
- 
- 
- 
- 
+int
+float 
+bool
+double
+char 
+unsigned int
  
  
 2) for each primitive type, write out 3 variable declarations inside the variableDeclaration() function on line 59.
@@ -56,10 +56,27 @@ void variableDeclarations()
 {
     //example:
     int number = 2; //declaration of a primitive named 'number' with an initial value of '2'
-    
-    
-    
-    ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+    int x = 5;
+    int y = 4;
+    int z = 3;
+    float num = 2.0f;
+    float whiz = 1.5f;
+    float avg = 95.0f;
+    bool result = true;
+    bool isOn = false;
+    bool light = true;
+    double take = 5.8;
+    double mass = 6.5;
+    double gravity = 9.8;
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+    unsigned int size = 235;
+    unsigned int length = 16;
+    unsigned int width = 12;
+
+
+    ignoreUnused(number, x, y, z, num, whiz, avg, result, isOn, light, take, mass, gravity, a, b, c, size, length, width); //passing each variable declared to the ignoreUnused() function
 }
 /*
  10 functions
@@ -74,43 +91,77 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 /*
  1)
  */
-
+float calculateProfit(float revenue, float expense)
+{
+    ignoreUnused(revenue, expense);
+    return{};
+}
 /*
  2)
  */
-
+void rotate(int degree)
+{
+    ignoreUnused(degree);
+}
 /*
  3)
  */
-
+int totalCustomers(int entered, int exited)
+{
+    ignoreUnused(entered, exited);
+    return{};
+}
 /*
  4)
  */
-
+void open()
+{
+}
 /*
  5)
  */
-
+bool hasGas(int milesDriven, int tankCapacity = 10)
+{
+  ignoreUnused(milesDriven, tankCapacity);
+  return{};
+}
 /*
  6)
  */
-
+void jump(int direction = 90, int length = 5)
+{
+    ignoreUnused(direction, length);
+}
 /*
  7)
  */
-
+float totalChange(float initial = 0.0f, float last = 100.0f)
+{
+    ignoreUnused(initial, last);
+    return{};
+}
 /*
  8)
  */
-
+bool finalDecision(int numReasons, int value, int excuses = 0)
+{
+    ignoreUnused(numReasons, value, excuses);
+    return{};
+}
 /*
  9)
  */
-
+float volumeOfWater(float rain, float area, float time)
+{
+    ignoreUnused(rain, area, time);
+    return{};
+}
 /*
  10)
  */
-
+void finish()
+{
+}
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -131,27 +182,27 @@ int main()
     auto carRented = rentACar(6, 2); 
     
     //1)
-    
+    auto profit = calculateProfit(100.0f, 75.6f);
     //2)
-    
+    rotate(90);
     //3)
-    
+    auto customers = totalCustomers(150, 10);
     //4)
-    
+    open();
     //5)
-    
+    auto stillRunning = hasGas(100, 8);
     //6)
-    
+    jump(45, 100);
     //7)
-    
+    auto delta = totalChange();
     //8)
-    
+    auto choice = finalDecision(5, 100, 3);
     //9)
-    
+    auto poolLevel = volumeOfWater(8.2f, 10.1f, 180.0f);
     //10)
+    finish();
     
-    
-    ignoreUnused(carRented);
+    ignoreUnused(carRented, profit, customers, stillRunning, delta, choice, poolLevel);
     std::cout << "good to go!" << std::endl;
     return 0;    
 }
